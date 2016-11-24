b97de3fb30ea548e7b06a93a606456997226a705
brussell98 <russellbrandon1999@gmail.com>
2016-11-24 15:00:37 -0600

    cooldown, pagination, allow custom logger an chatHandler

--- examples/test/commands/GeneralCommands.js
+++ examples/test/commands/GeneralCommands.js
@@ -23,6 +23,8 @@ class GeneralCommands extends AbstractCommandPlugin {
 					return reject(error);
 				for (let file of files) {
 					try {
+						if (!file.endsWith('.js'))
+							continue;
 						let command = new (require(__dirname + '/general/' + file))();
 						command.load(this).then(command => {
 							this.commands[command.name] = command;
@@ -37,8 +39,8 @@ class GeneralCommands extends AbstractCommandPlugin {
 	}
 
 	handle(message) {
-		if (message.content[0] === '!') {
-			let command = message.content.substr(1);
+		if (message.content.startsWith('m.')) {
+			let command = message.content.substr(2);
 			for (let name in this.commands) {
 				if (command.startsWith(name))
 					return this.commands[name].handle(message, command.replace(name, '').trim());
--- examples/test/commands/general/Ping.js
+++ examples/test/commands/general/Ping.js
@@ -10,6 +10,8 @@ class PingCommand extends AbstractCommand {
 	}
 
 	handle(message) {
+		if (this.userOnCooldown(message.author.id, 3000))
+			return this.sendMessage(message, 'You can only use this command every 3 seconds.', { deleteAfter: 3000, deleteTrigger: true });
 		this.sendMessage(message, 'pong!');
 	}
 }
