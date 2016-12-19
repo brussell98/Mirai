1b8c4b0dc9669314b4f6ddd2503ae34cdc269fea
brussell98 <russellbrandon1999@gmail.com>
2016-12-19 00:45:46 -0600

    Command.sendMessage resolves with message

--- examples/test/commands/general/Ping.js
+++ examples/test/commands/general/Ping.js
@@ -12,7 +12,9 @@ class PingCommand extends AbstractCommand {
 	handle(message) {
 		if (this.userOnCooldown(message.author.id, 3000))
 			return this.sendMessage(message, 'You can only use this command every 3 seconds.', { deleteAfter: 3000, deleteTrigger: true });
-		this.sendMessage(message, 'pong!');
+		this.sendMessage(message, 'pong in ??ms').then(msg => {
+			msg.edit(`pong in ${(msg.timestamp - message.timestamp).toLocaleString()}ms`);
+		});
 	}
 }
 
