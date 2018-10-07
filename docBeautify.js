e31abd9ae9289f2acd59d5e33578b50e869d49c6
Brussell <brussell98@users.noreply.github.com>
2018-10-06 22:31:26 -0500

    Update Mongoose example (#27)
    Also clean up docBeautify.js

--- docBeautify.js
+++ docBeautify.js
@@ -1,13 +1,15 @@
-var beautify_html = require('js-beautify').html,
-	fs = require('fs');
+const beautify_html = require('js-beautify').html;
+const fs = require('fs');
 
 fs.readdir(__dirname + '/docs/', (error, files) => {
 	if (error)
 		throw error;
+
 	files.filter(f => f.endsWith('.html')).map((filename, i) => {
 		fs.readFile(__dirname + '/docs/' + filename, 'utf8', (err, data) => {
 			if (err)
 				throw err;
+
 			fs.writeFile(__dirname + '/docs/' + filename, beautify_html(data, {
 				brace_style: 'collapse',
 				indent_with_tabs: true,
@@ -16,6 +18,7 @@ fs.readdir(__dirname + '/docs/', (error, files) => {
 			}), e => {
 				if (e)
 					throw e;
+
 				if (i === files.length - 1) {
 					console.log('HTML files beautified');
 					process.exit(0);
